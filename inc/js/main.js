874233ea4f9574a50b5a9d5d76e9cff7298ac087
Ezra <56590002+EzraIndigo@users.noreply.github.com>
2020-07-23 13:18:00 +0100

    Upload panel progress ++

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,4 +1,8 @@
 // Used to toggle the menu on small screens when clicking on the menu button
+
+
+
+// UI RELATED ACTIONS ---------------------------------------
 function myFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
@@ -7,3 +11,21 @@ function myFunction() {
       x.className = x.className.replace(" w3-show", "");
     }
   }
+
+
+  function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-light-blue", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " w3-light-blue";
+  }
+
+
+
